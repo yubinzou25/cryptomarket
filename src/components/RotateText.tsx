d05b7a20e62c3fc1f7895cc29b0ef224963c1107
Yubin Zou <yubinzou@Yubins-iMac.local>
2024-02-13 22:45:10 -0500

    simplify the code, and make more scalable

--- src/components/RotateText.tsx
+++ src/components/RotateText.tsx
@@ -1,42 +1,29 @@
 import { useEffect, useState } from 'react'
-// 0   =>   1    => 2  => 0
-// out => behind => in => out
-// 2s => 0.1 => 0.9s  => 2s
-//   full   gradual  gradual
 function RotateText({itemList} : {itemList: {text: string, color:string}[]}) {
-    const stateList = ["letter out", "letter behind", "letter in"];
+    const letterNumr = 3;
     const [displayIdx, setdisplayIdx] = useState(0);
     const [letterIdx, setLetterIdx] = useState(0);
-    const [behindIdx, setBehindIdx] = useState(-1);
     useEffect(() => {
         const id = setInterval(() => {
             setdisplayIdx(prevIdx => {
                 const nextIdx = (prevIdx + 1) % itemList.length;
-                setBehindIdx(nextIdx);
                 return nextIdx;
             });
             setLetterIdx(0);
-            setTimeout(() => setBehindIdx(-1), 300);
         }, 2000);
         return () => {
             clearInterval(id);
         };
       }, []);
     useEffect(() => {
-        if(letterIdx === 0){
-            setTimeout(() => {
-                setLetterIdx(1)
-            }, 200)
-        }else if(letterIdx === 1){
-            setTimeout(() => {
-                setLetterIdx(2)
-            }, 200)
-        }else if(letterIdx === 2){
-            setTimeout(() => {
-                setLetterIdx(3)
-            }, 200)
+        const nextLetterIdx = (letterIdx + 1) % (letterNumr + 1);
+        if(nextLetterIdx !== 0){
+            const timer = setTimeout(() => {
+                setLetterIdx(nextLetterIdx);
+            }, 200);
+            return () => clearTimeout(timer);
         }
-    }, [letterIdx])
+    }, [letterIdx, letterNumr])
   return (
     <>
         <span className="rotating-text">
@@ -48,10 +35,20 @@ function RotateText({itemList} : {itemList: {text: string, color:string}[]}) {
                                 {item.text.split('').map((char:string, charId:number) => {
                                     return <span 
                                             className={
-                                                displayIdx === textId ?
-                                                letterIdx <= charId ? stateList[1]: stateList[2]: 
-                                                displayIdx === (textId + 1) % itemList.length ?
-                                                letterIdx <= charId ? stateList[2]: stateList[0]: stateList[0]
+                                                // If the current item is the one being displayed
+                                            displayIdx === textId ? (
+                                                // If the current letter is behind or in front of the rotating letter
+                                                letterIdx <= charId ? "letter behind" : "letter in"
+                                            ) : (
+                                                // If the next item to be displayed is the one containing this letter
+                                                displayIdx === (textId + 1) % itemList.length ? (
+                                                    // If the current letter is behind or in front of the rotating letter
+                                                    letterIdx <= charId ? "letter in" : "letter out"
+                                                ) : (
+                                                    // Otherwise, this letter is not involved in the rotation
+                                                    "letter out"
+                                                )
+                                            )
                                             }
                                             key={charId}>
                                                 {char}
@@ -65,26 +62,6 @@ function RotateText({itemList} : {itemList: {text: string, color:string}[]}) {
         </span>
     </>
   );
-}
-
-function RotateLetter({letters, isDisplay}: {letters: string[], isDisplay: boolean}) {
-    const stateList = ["letter out", "letter behind", "letter in"];
-    const [letterState, setLetterState] = useState(new Array(letters.length).fill(0))
-    useEffect(() => {
-
-    }, [isDisplay])
-    console.log(letterState);
-    return (
-        <>
-            {letters.map((char:string, charId:number) => {
-                return <span 
-                        className={stateList[letterState[charId]]}
-                        key={charId}>
-                            {char}
-                        </span>;
-            })}
-        </>
-    );
 }
 
 export default RotateText
