cbf4fcaf368dae41c77f9114b0fe4c1eee8306d6
zouyubin <zouyubin@zouyubins-MacBook-Pro.local>
2024-02-28 23:44:50 -0500

    setup startTrade

--- src/components/pages/homepage/StartTrade.tsx
+++ src/components/pages/homepage/StartTrade.tsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 function StartTrade() {
   return (
-    <div className="text-2xl lg:text-3xl xl:text-4xl font-semibold">
-        StartTrade
+    <div className="flex flex-col space-y-20 bg-gray-50 text-center">
+        <div className="text-1xl lg:text-2xl xl:text-3xl font-semibold">
+            Start Trade in 1 Minute
+        </div>
+        <div className="flex flex-row w-3/4 justify-between">
+
+        </div>
+        <div>input</div>
+        <div className="flex flex-row">
+        </div>
     </div>
   )
 }
