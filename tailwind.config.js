8372d14da6085d950da7dffb34aea9afadec3cf4
Yubin Zou <yubinzou@Yubins-iMac.local>
2024-03-01 22:54:10 -0500

    get back to work

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,17 +10,8 @@ export default {
       'sm': '500px',
       'md': '800px',
       'lg': '1200px',
-      'xl': '1400px',
+      'xl': '1800px',
     }
-  },
-  // plugins: [
-  //   plugin(function({ addBase, theme }) {
-  //     addBase({
-  //       'h1': { fontSize: theme('fontSize.2xl') },
-  //       'h2': { fontSize: theme('fontSize.xl') },
-  //       'h3': { fontSize: theme('fontSize.lg') },
-  //     })
-  //   })
-  // ]
+  }
 }
 
