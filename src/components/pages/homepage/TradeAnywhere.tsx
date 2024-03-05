6496faecdaef8801de1efb090aba3def6052969a
zouyubin <zouyubin@zouyubins-mbp.wifi.mathworks.com>
2024-03-05 18:13:15 -0500

    layout the download image

--- src/components/pages/homepage/TradeAnywhere.tsx
+++ src/components/pages/homepage/TradeAnywhere.tsx
@@ -4,26 +4,25 @@ import LinkButton from "../../util/LinkButton";
 function TradeAnywhere() {
   return (
     <>
-      <div className="flex flex-row justify-evenly">
-        <div className="flex flex-col just">
+      <div className="flex flex-col lg:flex-row lg:space-x-20 justify-evenly">
+        <div className="flex flex-col mb-20">
           <div className="text-3xl lg:text-4xl text-center font-semibold">
             Trade Anywhere
             <div className="text-xl text-gray-500 font-light mt-5">
               Stay in the know with our app and desktop client.
             </div>
           </div>
-          <div className="flex flex-wrap">
-          </div>
-          
         </div>
+        <div className="flex justify-center">
         <div
-        className="bg-no-repeat bg-cover bg-center"
-          style={{
-              backgroundImage: `url("/image/download.png")`,
-              width:'629px',
-              height:'480px'
-          }}
+          className="bg-no-repeat bg-cover bg-center"
+            style={{
+                backgroundImage: `url("/image/download.png")`,
+                width:'629px',
+                height:'480px'
+            }}
           />
+        </div>
     </div>
 
     <div className="flex flex-col justify-between gap-y-10">
