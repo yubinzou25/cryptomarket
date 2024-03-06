d66156d6a8445efb48a46b3ceac9b76d1dc6eda3
zouyubin <zouyubin@zouyubins-mbp.wifi.mathworks.com>
2024-03-06 18:30:18 -0500

    reuse util chart

--- src/components/util/Chart.tsx
+++ src/components/util/Chart.tsx
@@ -1,25 +1,23 @@
 
 import {Area, AreaChart, ResponsiveContainer, Tooltip, XAxis, YAxis} from 'recharts';
-function Chart(data:any) {
-  
+function Chart({data}:{data: any}) {
   return (
-    <div className="flex justify-center">
-      <ResponsiveContainer width={"100%"} height={500}>
+    <ResponsiveContainer width={"100%"} height={500}>
       <AreaChart data={data}
       margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
         <defs>
-            <linearGradient id="colorPrice" x1="0" y1="0" x2="0" y2="1">
+          <linearGradient id="colorPrice" x1="0" y1="0" x2="0" y2="1">
             <stop
-                offset="0%"
-                stopColor="#00E5DB"
-                stopOpacity={0.3}
+              offset="0%"
+              stopColor="#00E5DB"
+              stopOpacity={0.3}
             />
             <stop
-                offset="100%"
-                stopColor="#00E5DB"
-                stopOpacity={0}
+              offset="100%"
+              stopColor="#00E5DB"
+              stopOpacity={0}
             />
-            </linearGradient>
+          </linearGradient>
         </defs>
       <XAxis dataKey="time"
         axisLine={false}
@@ -44,7 +42,6 @@ function Chart(data:any) {
       />
       </AreaChart>
       </ResponsiveContainer>
-    </div>
   )
 }
 
