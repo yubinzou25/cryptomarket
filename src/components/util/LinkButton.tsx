90565d3e642f8ddc7ff337a4732696f335b55033
zouyubin <zouyubin@zouyubins-MacBook-Pro.local>
2024-03-08 17:48:29 -0500

    fix linkbutton

--- src/components/util/LinkButton.tsx
+++ src/components/util/LinkButton.tsx
@@ -1,10 +1,11 @@
+import { Link } from "react-router-dom"
 
 function LinkButton({to, buttonText, primaryColor}:{to:string, buttonText:string, primaryColor:boolean}) {
   return (
-    <button className={`font-medium rounded-lg px-4 py-2 whitespace-nowrap \
+    <Link className={`font-medium rounded-lg px-4 py-2 whitespace-nowrap \
     ${primaryColor? 'bg-primary-default hover:bg-primary-hover': 'hover:bg-gray-100'}`} to={to}>
         {buttonText}
-    </button>
+    </Link>
   )
 }
 
