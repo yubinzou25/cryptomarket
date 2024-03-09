42f40d58f4f34813c1175b4a44ecb1c8f2125282
zouyubin <zouyubin@zouyubins-MacBook-Pro.local>
2024-03-08 23:55:52 -0500

    button remain toggled when mouse leave

--- src/components/pages/homepage/PriceChart.tsx
+++ src/components/pages/homepage/PriceChart.tsx
@@ -1,14 +1,13 @@
 
 import { useMemo, useState } from 'react';
-import { useGetCoinsQuery, useGetCoinDetailQuery, useGetCoinPriceHistoryQuery } from '../../../api/cryptoApi';
+import { useGetCoinsQuery, useGetCoinPriceHistoryQuery } from '../../../api/cryptoApi';
 import Chart from '../../util/Chart';
 import CoinButton from '../../util/CoinButton';
 function PriceChart() {
   const [requestUUID, setRequestUUID] = useState('Qwsogvtv82FCd');
-  const {data: coins} = useGetCoinsQuery(3);
-  console.log(coins);
+  const {data: coinsData} = useGetCoinsQuery(4);
+  console.log(coinsData);
   const {data:priceData} = useGetCoinPriceHistoryQuery(requestUUID);
-
   const priceHistory = useMemo(() => {
     const history =  priceData?.data?.history || [];
     return history.map((val:any, _index:any) => {
@@ -16,17 +15,16 @@ function PriceChart() {
         hour: 'numeric',
         minute: '2-digit',
         hour12: false,
-      }).format(new Date(val.timestamp));
-      const price = parseFloat(val.price);
-      return {price:price.toFixed(2), time: formattedTime}
+      }).format(new Date(val.timestamp * 1000));
+      return {price:val.price, time: formattedTime}
     })
   }, [priceData]) 
   return (
     <div className="flex flex-col md:flex-row justify-between">
       <div className="flex flex-row md:flex-col md:w-1/5 justify-evenly">
-        <CoinButton name='BTC/USDT' uuid="Qwsogvtv82FCd" setRequestUUID={setRequestUUID}/>
-        <CoinButton name='ETH/USDT' uuid="razxDUgYGNAdQ" setRequestUUID={setRequestUUID}/>
-        <CoinButton name='ARB/USDT' uuid="HIVsRcGKkPFtW" setRequestUUID={setRequestUUID}/>
+        <CoinButton name='BTC/USDT' requestUUID={requestUUID} uuid="Qwsogvtv82FCd" setRequestUUID={setRequestUUID}/>
+        <CoinButton name='ETH/USDT' requestUUID={requestUUID} uuid="razxDUgYGNAdQ" setRequestUUID={setRequestUUID}/>
+        <CoinButton name='ARB/USDT' requestUUID={requestUUID} uuid="HIVsRcGKkPFtW" setRequestUUID={setRequestUUID}/>
       </div>
       <Chart data={priceHistory}/>
     </div>
--- src/components/util/CoinButton.tsx
+++ src/components/util/CoinButton.tsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
 
-function CoinButton({name, uuid, setRequestUUID}:{name:string, uuid:string, setRequestUUID: (uuid:string) => void}) {
+function CoinButton({name, uuid, requestUUID, setRequestUUID}:{name:string, uuid:string, requestUUID:string, setRequestUUID: (uuid:string) => void}) {
   const handleButtonClick = () => {
     setRequestUUID(uuid);
   };
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+  const [isHovered, setIsHovered] = useState(false);
   return (
     <button 
-    className={`font-medium rounded-full px-4 py-2 whitespace-nowrap w-4/5 bg-gray-100 
-    hover:w-full hover:bg-inherit hover:border-black hover:border-2 duration-300`}
+    className={`font-medium rounded-full px-4 py-2 whitespace-nowrap 
+    ${isHovered || uuid === requestUUID? "w-full bg-inherit border-black border-2":
+    "w-4/5 bg-gray-100"} duration-300`}
     onClick={handleButtonClick}
+    onMouseEnter={handleMouseEnter}
+    onMouseLeave={handleMouseLeave}
     >
       <div className="flex flex-row justify-evenly items-center">
           <img className="w-10 h-10 relative overflow-hidden rounded-full" src="/image/BTC.png"/>
