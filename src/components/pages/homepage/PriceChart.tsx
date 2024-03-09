290516589f1827696d721b6325f99d34ae7fdca7
zouyubin <zouyubin@zouyubins-MacBook-Pro.local>
2024-03-08 23:33:23 -0500

    switch chart when click coin button

--- src/components/pages/homepage/PriceChart.tsx
+++ src/components/pages/homepage/PriceChart.tsx
@@ -1,10 +1,13 @@
 
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 import { useGetCoinsQuery, useGetCoinDetailQuery, useGetCoinPriceHistoryQuery } from '../../../api/cryptoApi';
 import Chart from '../../util/Chart';
 import CoinButton from '../../util/CoinButton';
 function PriceChart() {
-  const {data:priceData} = useGetCoinPriceHistoryQuery('Qwsogvtv82FCd');
+  const [requestUUID, setRequestUUID] = useState('Qwsogvtv82FCd');
+  const {data: coins} = useGetCoinsQuery(3);
+  console.log(coins);
+  const {data:priceData} = useGetCoinPriceHistoryQuery(requestUUID);
 
   const priceHistory = useMemo(() => {
     const history =  priceData?.data?.history || [];
@@ -21,9 +24,9 @@ function PriceChart() {
   return (
     <div className="flex flex-col md:flex-row justify-between">
       <div className="flex flex-row md:flex-col md:w-1/5 justify-evenly">
-        <CoinButton to='/' buttonText='BTC/USDT'/>
-        <CoinButton to='/' buttonText='ETH/USDT'/>
-        <CoinButton to='/' buttonText='ARB/USDT'/>
+        <CoinButton name='BTC/USDT' uuid="Qwsogvtv82FCd" setRequestUUID={setRequestUUID}/>
+        <CoinButton name='ETH/USDT' uuid="razxDUgYGNAdQ" setRequestUUID={setRequestUUID}/>
+        <CoinButton name='ARB/USDT' uuid="HIVsRcGKkPFtW" setRequestUUID={setRequestUUID}/>
       </div>
       <Chart data={priceHistory}/>
     </div>
--- src/components/util/CoinButton.tsx
+++ src/components/util/CoinButton.tsx
@@ -1,20 +1,22 @@
-import { Link } from 'react-router-dom'
 
-function CoinButton({to, buttonText}:{to:string, buttonText:string}) {
+function CoinButton({name, uuid, setRequestUUID}:{name:string, uuid:string, setRequestUUID: (uuid:string) => void}) {
+  const handleButtonClick = () => {
+    setRequestUUID(uuid);
+  };
   return (
-    <Link 
+    <button 
     className={`font-medium rounded-full px-4 py-2 whitespace-nowrap w-4/5 bg-gray-100 
     hover:w-full hover:bg-inherit hover:border-black hover:border-2 duration-300`}
-    to={to}
+    onClick={handleButtonClick}
     >
       <div className="flex flex-row justify-evenly items-center">
           <img className="w-10 h-10 relative overflow-hidden rounded-full" src="/image/BTC.png"/>
           <div className="flex flex-col">
-              <div>{buttonText}</div>
+              <div>{name}</div>
               <div>67080.5</div>
           </div>
       </div>
-  </Link>
+  </button>
   )
 }
 
