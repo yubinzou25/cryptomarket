ff2826609e548a37e3f318c563de521c22c92c0f
zouyubin <zouyubin@zouyubins-MacBook-Pro.local>
2024-03-10 16:52:42 -0400

    add group button

--- src/components/pages/homepage/PriceChart.tsx
+++ src/components/pages/homepage/PriceChart.tsx
@@ -5,8 +5,7 @@ import Chart from '../../util/Chart';
 import CoinButton from '../../util/CoinButton';
 function PriceChart() {
   const [requestUUID, setRequestUUID] = useState('Qwsogvtv82FCd');
-  const {data: coinsData} = useGetCoinsQuery(4);
-  console.log(coinsData);
+  // const {data: coinsData} = useGetCoinsQuery(4);
   const {data:priceData} = useGetCoinPriceHistoryQuery(requestUUID);
   const priceHistory = useMemo(() => {
     const history =  priceData?.data?.history || [];
@@ -20,13 +19,28 @@ function PriceChart() {
     })
   }, [priceData]) 
   return (
-    <div className="flex flex-col md:flex-row justify-between">
-      <div className="flex flex-row md:flex-col md:w-1/5 justify-evenly">
-        <CoinButton name='BTC/USDT' requestUUID={requestUUID} uuid="Qwsogvtv82FCd" setRequestUUID={setRequestUUID}/>
-        <CoinButton name='ETH/USDT' requestUUID={requestUUID} uuid="razxDUgYGNAdQ" setRequestUUID={setRequestUUID}/>
-        <CoinButton name='ARB/USDT' requestUUID={requestUUID} uuid="HIVsRcGKkPFtW" setRequestUUID={setRequestUUID}/>
+    <div className="flex flex-col">
+      <div className="flex flex-row justify-end">
+      <div className="inline-flex rounded-lg">
+        <button className="py-3 px-4 items-center gap-x-2 -ms-px first:rounded-s-lg first:ms-0 last:rounded-e-lg text-sm font-medium focus:z-10 border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
+          1D
+        </button>
+        <button className="py-3 px-4 items-center gap-x-2 -ms-px first:rounded-s-lg first:ms-0 last:rounded-e-lg text-sm font-medium focus:z-10 border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
+          7D
+        </button>
+        <button className="py-3 px-4 items-center gap-x-2 -ms-px first:rounded-s-lg first:ms-0 last:rounded-e-lg text-sm font-medium focus:z-10 border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
+          30D
+        </button>
       </div>
-      <Chart data={priceHistory}/>
+      </div>
+      <div className="flex flex-col md:flex-row justify-between">
+        <div className="flex flex-row md:flex-col md:w-1/5 justify-evenly">
+          <CoinButton name='BTC' requestUUID={requestUUID} uuid="Qwsogvtv82FCd" setRequestUUID={setRequestUUID}/>
+          <CoinButton name='ETH' requestUUID={requestUUID} uuid="razxDUgYGNAdQ" setRequestUUID={setRequestUUID}/>
+          <CoinButton name='USDT' requestUUID={requestUUID} uuid="HIVsRcGKkPFtW" setRequestUUID={setRequestUUID}/>
+        </div>
+        <Chart data={priceHistory}/>
+      </div>
     </div>
   )
 }
