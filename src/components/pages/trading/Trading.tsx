62a0cf9f1975e48a6a8b6a2efd081f1d72d3a867
Yubin Zou <yubinzou@Yubins-iMac.local>
2024-04-06 22:13:31 -0400

    add number input block

--- src/components/pages/trading/Trading.tsx
+++ src/components/pages/trading/Trading.tsx
@@ -5,9 +5,25 @@ function Trading() {
  const { coinId } = useParams<{ coinId: string }>();
 
   return (
-   <div className="mt-20">
+   <div className="flex flex-row mt-20">
    <TradingChart
      coinId={coinId || 'Qwsogvtv82FCd'}/>
+   <div className="flex flex-col">
+    <div className="flex flex-row">
+     <button className="p-5 broder-1">Buy BTC</button>
+     <button className="p-5">Sell BTC</button>
+    </div>
+    <div className="flex flex-col">
+     <div className="flex flex-row space-x-20">
+      <div>Invest in</div>
+      <input type="text" placeholder="$0.00" className="py-1.5 pl-7 pr-10 rounded-md bg-gray-100 ring-1 ring-inset ring-gray-500 text-gray-900"></input>
+     </div>
+     <div className="flex flex-row space-x-20">
+      <div>Est. Quantity</div>
+      <div>0</div>
+     </div>
+    </div>
+   </div>
    </div>
   );
 }
