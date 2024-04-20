6887ac5958c7d4ef0e62a6b760e4a05093278bbc
Yubin Zou <yubinzou@Yubins-iMac.local>
2024-04-20 11:51:52 -0400

    adjust trading dialogue

--- src/components/pages/trading/Trading.tsx
+++ src/components/pages/trading/Trading.tsx
@@ -8,20 +8,31 @@ function Trading() {
    <div className="flex flex-row mt-20">
    <TradingChart
      coinId={coinId || 'Qwsogvtv82FCd'}/>
-   <div className="flex flex-col">
-    <div className="flex flex-row">
+   <div className="flex flex-col shadow-xl">
+    <div className="flex flex-row justify-evenly">
      <button className="p-5 broder-1">Buy BTC</button>
      <button className="p-5">Sell BTC</button>
     </div>
     <div className="flex flex-col">
-     <div className="flex flex-row space-x-20">
+    <div className="flex flex-row justify-evenly">
       <div>Invest in</div>
-      <input type="text" placeholder="$0.00" className="py-1.5 pl-7 pr-10 rounded-md bg-gray-100 ring-1 ring-inset ring-gray-500 text-gray-900"></input>
+      <select id="currency" name="currency" className="w-1/2 py-1.5 pl-7 pr-10 h-full rounded-md border-0 bg-transparent py-0 pl-2 pr-7 text-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm">
+        <option>USD</option>
+        <option>CAD</option>
+        <option>EUR</option>
+      </select>
      </div>
-     <div className="flex flex-row space-x-20">
+     <div className="flex flex-row justify-evenly">
+      <div>Amount</div>
+      <input type="text" placeholder="$0.00" className="w-1/2 py-1.5 pl-7 pr-10 rounded-md bg-gray-100 ring-1 ring-inset ring-gray-500 text-gray-900"></input>
+     </div>
+     <div className="flex flex-row justify-evenly">
       <div>Est. Quantity</div>
-      <div>0</div>
+      <span className="w-1/2 py-1.5 pl-7 pr-10">0</span>
      </div>
+     <button className="font-medium rounded-lg px-4 py-2 mx-5 whitespace-nowrap bg-primary-default hover:bg-primary-hover">
+        place order
+    </button>
     </div>
    </div>
    </div>
